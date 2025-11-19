2b00294ba1814ee964a3a40b9555490fe005f422
Vadim Ponomarev <59008533+holylance98@users.noreply.github.com>
2025-11-20 08:04:25 +1000

    Added

--- quality.sh
+++
@@ -1,9 +0,0 @@
-mkdir "output"
-chmod -vR 777 "output"
-for file in *.m4a; do
-    ffmpeg -i "$file" -qscale:a 9 "${file//.m4a/.ogg}";
-    rm -vr "$file";
-done
-for file in *.ogg; do
-    ffmpeg -y -i "$file" -qscale:a 9 "output/$file";
-done
